44d0a7fdccb214ee614358215cb4f72bb05f55c7
Angel Ramirez <angel.ramirez@wizeline.com>
2019-11-15 16:34:24 -0600

    Add ui for places

--- app/src/main/java/io/pulque/fleu/ui/ProfileActivity.kt
+++ app/src/main/java/io/pulque/fleu/ui/ProfileActivity.kt
@@ -1,13 +1,19 @@
 package io.pulque.fleu.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import io.pulque.fleu.R
+import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        address.setOnClickListener{
+            startActivity(Intent(this, PlaceAddressActivity::class.java))
+        }
     }
 }
